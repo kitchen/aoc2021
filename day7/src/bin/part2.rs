f35f0aa58ce07afb289fbaf23e4ccc1dcb06711f
Jeremy Kitchen <kitchen@kitchen.io>
2022-01-05 18:54:33 -0800

    ok, part2 is easy just change the weights

---
+++ day7/src/bin/part2.rs
@@ -0,0 +1,40 @@
+#![feature(stdin_forwarders)]
+
+use day7::crabs;
+use std::io::{self, Read};
+
+fn main() {
+    let mut buf = Vec::new();
+    io::stdin()
+        .read_to_end(&mut buf)
+        .expect("unable to read to end or something");
+
+    let (_, mut crabs) = crabs(&buf).expect("unable to parse crabs");
+    println!("starting crabs: {:?}", crabs);
+
+    // ok, I think this is a "weighted mean" problem.
+    // the thing I'm averaging is the distance of each crab from given point
+    // weighted by the number of crabs at that point
+    // that's how I'm going to approach it at first
+    // I'll just iterate through min()..=max() and find the one with the ... smallest weighted mean?
+    let min_pos = *crabs.iter().min().unwrap();
+    let max_pos = *crabs.iter().max().unwrap();
+    let num_crabs: i32 = crabs.len() as i32;
+    
+    let mut best_pos = 0;
+    // there's a better way to do this but I'm lazy
+    let mut best_fuel = 1231231231;
+    
+    for pos in min_pos..=max_pos {
+        let total_fuel = crabs.iter().fold(0,|acc, crab| {
+            (1..=((crab-pos).abs())).sum::<i32>() + acc
+        });
+        println!("pos: {}, total fuel: {}", pos, total_fuel);
+        if best_fuel > total_fuel {
+            best_fuel = total_fuel;
+            best_pos = pos;
+        }
+    }
+    
+    println!("best fuel is at pos {}: {}", best_pos, best_fuel);
+}
