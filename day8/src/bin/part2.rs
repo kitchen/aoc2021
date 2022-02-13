61b5a93082e5fafb8f94ba69e203c9ab99722e61
Jeremy Kitchen <kitchen@kitchen.io>
2022-02-12 22:19:22 -0800

    holy hell this is so ugly I am almost embarrased to post it

--- day8/src/bin/part2.rs
+++ day8/src/bin/part2.rs
@@ -1,15 +1,60 @@
 #![feature(stdin_forwarders)]
 
-// use day8::{
-//     parse_patterns,
-// };
+use day8::*;
 use std::io::{self, Read};
 
 fn main() {
-    let mut buf = Vec::new();
+    let mut buf = String::new();
     io::stdin()
-        .read_to_end(&mut buf)
+        .read_to_string(&mut buf)
         .expect("unable to read to end or something");
+
+    let (_, lines) = parse_lines(&buf).unwrap();
+    let mut total = 0;
+    for (patterns, numbers) in lines {
+        let one = find_one(&patterns);
+        let two = find_two(&patterns);
+        let three = find_three(&patterns);
+        let four = find_four(&patterns);
+        let five = find_five(&patterns);
+        let six = find_six(&patterns);
+        let seven = find_seven(&patterns);
+        let eight = find_eight(&patterns);
+        let nine = find_nine(&patterns);
+        let zero = find_zero(&patterns);
+        
+        let value = numbers.iter().fold(0, |acc, x| {
+            let acc = acc * 10;
+            let mut foo = 0;
+            if x == one {
+                foo = 1
+            } else if x == two {
+                foo = 2
+            } else if x == three {
+                foo = 3
+            } else if x == four {
+                foo = 4
+            } else if x == five {
+                foo = 5
+            } else if x == six {
+                foo = 6
+            } else if x == seven {
+                foo = 7
+            } else if x == eight {
+                foo = 8
+            } else if x == nine {
+                foo = 9
+            } else if x == zero {
+                foo = 0
+            }
+            acc + foo
+        });
+        
+        total += value
+        
+    }
+    
+    println!("total of all of the numbers: {}", total);
 
 
 }
--- day8/src/lib.rs
+++ day8/src/lib.rs
@@ -35,20 +35,62 @@ pub fn parse_lines(input: &str) -> IResult<&str, Vec<(Vec<Digit>, Vec<Digit>)>> 
     separated_list1(tag("\n"), parse_line)(input)
 }
 
-pub fn find_seven<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
-    patterns.iter().find(|pattern| pattern.segments.len() == 3).unwrap()
-}
-
 pub fn find_one<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
     patterns.iter().find(|pattern| pattern.segments.len() == 2).unwrap()
+}
+
+pub fn find_two<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
+    let three = find_three(patterns);
+    let four = find_four(patterns);
+    let mut candidates = patterns.iter().filter(|pattern| pattern.segments.len() == 5 && pattern != &three);
+    candidates.find(|pattern| pattern.segments.difference(&four.segments).count() == 3).unwrap()
+}
+
+pub fn find_three<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
+    let one = find_one(patterns);
+    let mut candidates = patterns.iter().filter(|pattern| pattern.segments.len() == 5);
+    candidates.find(|pattern| pattern.segments.difference(&one.segments).count() == 3).unwrap()
 }
 
 pub fn find_four<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
     patterns.iter().find(|pattern| pattern.segments.len() == 4).unwrap()
 }
 
+pub fn find_five<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
+    let three = find_three(patterns);
+    let four = find_four(patterns);
+    let mut candidates = patterns.iter().filter(|pattern| pattern.segments.len() == 5 && pattern != &three);
+    candidates.find(|pattern| pattern.segments.difference(&four.segments).count() == 2).unwrap()
+}
+
+pub fn find_six<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
+    let one = find_one(patterns);
+    let five = find_five(patterns);
+    let upper_right = one.segments.difference(&five.segments).last().unwrap();
+    let mut candidates = patterns.iter().filter(|pattern| pattern.segments.len() == 6);
+    candidates.find(|pattern| !pattern.segments.contains(upper_right)).unwrap()
+}
+
+pub fn find_seven<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
+    patterns.iter().find(|pattern| pattern.segments.len() == 3).unwrap()
+}
+
 pub fn find_eight<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
     patterns.iter().find(|pattern| pattern.segments.len() == 7).unwrap()
+}
+
+pub fn find_nine<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
+    let four = find_four(patterns);
+    let six = find_six(patterns);
+    let mut candidates = patterns.iter().filter(|pattern| pattern.segments.len() == 6 && pattern != &six);
+    candidates.find(|pattern| pattern.segments.difference(&four.segments).count() == 2).unwrap()
+}
+
+pub fn find_zero<'a>(patterns: &'a Vec<Digit>) -> &'a Digit {
+    let four = find_four(patterns);
+    let six = find_six(patterns);
+    let mut candidates = patterns.iter().filter(|pattern| pattern.segments.len() == 6 && pattern != &six);
+    candidates.find(|pattern| pattern.segments.difference(&four.segments).count() == 3).unwrap()
 }
 
 #[cfg(test)]
@@ -56,60 +98,52 @@ mod tests {
     use super::*;
     
     #[test]
-    fn test_patterns() {
+    fn test_finds() {
         let case = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
-        let expected = vec!("acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab");
+        let one_string = "ab";
+        let (_, one_digit) = parse_digit(one_string).unwrap();
+        let two_string = "gcdfa";
+        let (_, two_digit) = parse_digit(two_string).unwrap();
+        let three_string = "fbcad";
+        let (_, three_digit) = parse_digit(three_string).unwrap();
+        let four_string = "eafb";
+        let (_, four_digit) = parse_digit(four_string).unwrap();
+        let five_string = "cdfbe";
+        let (_, five_digit) = parse_digit(five_string).unwrap();
+        let six_string = "cdfgeb";
+        let (_, six_digit) = parse_digit(six_string).unwrap();
+        let seven_string = "dab";
+        let (_, seven_digit) = parse_digit(seven_string).unwrap();
+        let eight_string = "acedgfb";
+        let (_, eight_digit) = parse_digit(eight_string).unwrap();
+        let nine_string = "cefabd";
+        let (_, nine_digit) = parse_digit(nine_string).unwrap();
+        let zero_string = "cagedb";
+        let (_, zero_digit) = parse_digit(zero_string).unwrap();
         
-        let mut expected_one_segments = HashSet::new();
-        expected_one_segments.insert('a');
-        expected_one_segments.insert('b');
-        let expected_one = Digit { segments: expected_one_segments };
+        let (_rest, digits) = parse_digits(case).unwrap();
         
-        let (_rest, res) = parse_digits(case).unwrap();
+        let found_one = find_one(&digits);
+        assert_eq!(found_one, &one_digit);
+        let found_two = find_two(&digits);
+        assert_eq!(found_two, &two_digit);
+        let found_three = find_three(&digits);
+        assert_eq!(found_three, &three_digit);
+        let found_four = find_four(&digits);
+        assert_eq!(found_four, &four_digit);
+        let found_five = find_five(&digits);
+        assert_eq!(found_five, &five_digit);
+        let found_six = find_six(&digits);
+        assert_eq!(found_six, &six_digit);
+        let found_seven = find_seven(&digits);
+        assert_eq!(found_seven, &seven_digit);
+        let found_eight = find_eight(&digits);
+        assert_eq!(found_eight, &eight_digit);
+        let found_nine = find_nine(&digits);
+        assert_eq!(found_nine, &nine_digit);
+        let found_zero = find_zero(&digits);
+        assert_eq!(found_zero, &zero_digit);
         
-        let res_one = &res[9];
-        assert_eq!(res_one, &expected_one);
-        
-        let found_one = find_one(&res);
-        assert_eq!(found_one, &expected_one);
-        
-        let mut expected_four_segments = HashSet::new();
-        expected_four_segments.insert('a');
-        expected_four_segments.insert('b');
-        expected_four_segments.insert('e');
-        expected_four_segments.insert('f');
-        let expected_four = Digit { segments: expected_four_segments };
-        
-        let found_four = find_four(&res);
-        assert_eq!(found_four, &expected_four);
-        
-        // seven
-        let mut expected_seven_segments = HashSet::new();
-        expected_seven_segments.insert('a');
-        expected_seven_segments.insert('b');
-        expected_seven_segments.insert('d');
-        let expected_seven = Digit { segments: expected_seven_segments };
-        
-        let found_seven = find_seven(&res);
-        assert_eq!(found_seven, &expected_seven);
-        
-        // eight
-        let mut expected_eight_segments = HashSet::new();
-        expected_eight_segments.insert('a');
-        expected_eight_segments.insert('b');
-        expected_eight_segments.insert('c');
-        expected_eight_segments.insert('d');
-        expected_eight_segments.insert('e');
-        expected_eight_segments.insert('f');
-        expected_eight_segments.insert('g');
-        let expected_eight = Digit { segments: expected_eight_segments };
-        
-        let found_eight = find_eight(&res);
-        assert_eq!(found_eight, &expected_eight);
-        
-        let input = "abc de | cba ed";
-        let (_rest, (patterns, numbers)) = parse_line(input).unwrap();
-        assert_eq!(patterns.len(), 2);
-        assert_eq!(numbers.len(), 2);
     }
+    
 }
