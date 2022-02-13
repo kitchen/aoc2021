52ae455f9be9edfe58af2f908ac9a25cd8d3c085
Jeremy Kitchen <kitchen@kitchen.io>
2022-02-12 23:10:51 -0800

    add tests to template lib

--- template/lib.rs
+++ template/lib.rs
@@ -7,3 +7,14 @@ use nom::IResult;
 pub fn parse(input: &[u8]) -> IResult<&[u8], Vec<i32>> {
     separated_list1(tag(","), i32)(input)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_finds() {
+
+    }
+}
