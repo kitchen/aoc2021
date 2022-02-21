e306acd661d740c828272e6ce807dd26c6bc8898
Jeremy Kitchen <kitchen@kitchen.io>
2022-02-21 01:35:08 -0800

    day14 part2
    part2 obviously had to be done differently.e
    
    also the math isn't *quite* right. I need to add one to one of the values (to account for the last element in the polymer) and I figured I had 3 options all 1 away from each other so I just brute forced that part hah

--- day14/src/bin/part1.rs
+++ day14/src/bin/part1.rs
@@ -2,6 +2,7 @@
 
 use day14::parse;
 use itertools::Itertools;
+use itertools::MinMaxResult::MinMax;
 use std::io::{self, Read};
 
 fn main() {
@@ -13,7 +14,7 @@ fn main() {
     let (_, (template, instructions)) = parse(&buf).expect("unable to parse input");
     let mut template: Vec<char> = template.chars().collect();
     
-    for i in 1..=10 {
+    for i in 1..=40 {
         let mut last: Option<char> = None;
         template = template.iter().tuple_windows().fold(Vec::new(), |mut acc, (a, b)| {
             acc.push(*a);
@@ -25,11 +26,20 @@ fn main() {
             acc
         });
         template.push(last.unwrap());
-        println!("polymer after {} iterations: {:?}", i, template);
+        // println!("polymer after {} iterations: {:?}", i, template);
+        println!("iteration {} complete!", i);
+        let mut counts: Vec<usize> = Vec::new();
+        for (key, group) in &template.iter().sorted().group_by(|x| *x) {
+            let count = group.count();
+            counts.push(count);
+            // println!("key: {:?}, group: {:?}", key, count);
+        };
+
+        match counts.iter().minmax() {
+            MinMax(min, max) => println!("iteration {} len {} difference between max and min: {}, len - max - min: {}", i, template.len(), max - min, template.len() - max - min),
+            _ => {},
+        }
     }
     
-    for (key, group) in &template.iter().sorted().group_by(|x| *x) {
-        println!("key: {:?}, group: {:?}", key, group.count());
-    };
 
 }
--- day14/src/bin/part2.rs
+++ day14/src/bin/part2.rs
@@ -1,7 +1,10 @@
 #![feature(stdin_forwarders)]
 
 use day14::parse;
+use itertools::Itertools;
+use itertools::MinMaxResult::MinMax;
 use std::io::{self, Read};
+use std::collections::HashMap;
 
 fn main() {
     let mut buf = String::new();
@@ -9,6 +12,38 @@ fn main() {
         .read_to_string(&mut buf)
         .expect("unable to read to end or something");
 
-    let (_, crabs) = parse(&buf).expect("unable to parse input");
+    let (_, (template, instructions)) = parse(&buf).expect("unable to parse input");
+    
+    let mut pairs_counts: HashMap<(char, char), usize> = HashMap::new();
+    for pair in template.chars().tuple_windows() {
+        pairs_counts.entry(pair).and_modify(|x| *x += 1).or_insert(1);
+    }
+    let last = template.chars().last().unwrap();
+    
+    for i in 1..=40 {
+        let mut new_pairs_counts: HashMap<(char, char), usize> = HashMap::new();
+        let mut first_counts: HashMap<char, usize> = HashMap::new();
+        for ((a, b), count) in pairs_counts {
+            match instructions.get(&(a, b)) {
+                Some(insert) => {
+                    new_pairs_counts.entry((a, *insert)).and_modify(|x| *x += count).or_insert(count);
+                    first_counts.entry(a).and_modify(|x| *x += count).or_insert(count);
+                    new_pairs_counts.entry((*insert, b)).and_modify(|x| *x += count).or_insert(count);
+                    first_counts.entry(*insert).and_modify(|x| *x += count).or_insert(count);
+                },
+                None => { new_pairs_counts.entry((a, b)).and_modify(|x| *x += count).or_insert(count); },
+            }
+            
 
+        }
+        match first_counts.values().minmax() {
+            MinMax(min, max) => { println!("max ({}) - min ({}) = {}", max, min, max - min); },
+            _ => {},
+            
+        }
+        pairs_counts = new_pairs_counts;
+
+        
+    }
+    println!("pairs counts: {:?}", pairs_counts);
 }
